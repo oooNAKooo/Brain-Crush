05e93d49e595cded4df0804fef05fd3c52f50c15
Samuel <121157661+oooNAKooo@users.noreply.github.com>
2023-11-14 22:05:15 +0300

    add_sudoku_code

--- Code/sudoku.py
+++ Code/sudoku.py
@@ -1,190 +1,137 @@
-# sudoku.py
 import tkinter as tk
 from tkinter import messagebox
-import random
-from database import Database
-from tictactoe import TicTacToeApp
-
 
 class SudokuGame:
-    def __init__(self):
-        self.board = [[0] * 9 for _ in range(9)]
-        self.initialize_board()
+    def __init__(self, parent, show_rules_func):
+        self.parent = parent
+        self.show_rules_func = show_rules_func
 
-    def initialize_board(self):
-        # Начальное состояние доски Судоку
-        # Ваш код для генерации начальной доски
-        pass
+        # Создание и отрисовка игрового поля
+        self.board = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
 
-    def make_move(self, row, col, value):
-        # Обработка хода игрока
-        if self.board[row][col] == 0:
-            self.board[row][col] = value
-            return True
-        else:
-            return False
+        self.entries = [[None for _ in range(9)] for _ in range(9)]
+        self.draw_board()
 
-    def is_board_full(self):
-        # Проверка, заполнена ли доска полностью
-        for row in self.board:
-            if 0 in row:
-                return False
-        return True
+    def draw_board(self):
+        frame = tk.Frame(self.parent)
+        frame.place(relx=0.5, rely=0.5, anchor="center")
 
-    def check_winner(self):
-        # Проверка победы
-        # Для Судоку, победы обычно не проверяются,
-        # так как игра завершается, когда доска полностью заполнена
-        return None
+        # Функция для валидации ввода
+        def validate_input(char, index, current_value):
+            return char.isdigit() and 0 < int(char) <= 9 and len(current_value) <= 1
 
-class SudokuApp:
-    def __init__(self, root, db, show_game_menu_callback):
-        self.root = root
-        self.db = db
-        self.show_game_menu_callback = show_game_menu_callback
+        vcmd = (frame.register(validate_input), '%S', '%i', '%P')
 
-        self.game_window = tk.Toplevel(root)
-        self.game_window.title("Судоку")
-        self.game_window.geometry("400x400")
-
-        self.frame = tk.Frame(self.game_window)
-        self.frame.grid(row=0, column=0, padx=10, pady=10)
-
-        self.game = SudokuGame()
-        self.create_board()
-
-    def create_board(self):
         for i in range(9):
             for j in range(9):
-                entry = tk.Entry(self.frame, width=3, font=("Arial", 16), justify="center")
-                entry.grid(row=i, column=j, padx=2, pady=2)
+                cell_value = self.board[i][j]
+                is_initial_value = cell_value != 0
 
-                # Заполнение начального состояния доски
-                value = self.game.board[i][j]
-                if value != 0:
-                    entry.insert(0, str(value))
-                    entry.config(state="disabled")
+                # Определение цвета текста в ячейке
+                text_color = "red" if is_initial_value else "black"
 
-                entry.bind("<FocusIn>", lambda event, row=i, col=j: self.on_entry_click(event, row, col))
-                entry.bind("<Key>", lambda event, row=i, col=j: self.on_key_press(event, row, col))
+                cell_entry = tk.Entry(frame, width=3, font=("Arial", 24), justify="center", validate="key", validatecommand=vcmd)
+                cell_entry.grid(row=i, column=j)
+                cell_entry.insert(0, str(cell_value))
 
-    def on_entry_click(self, event, row, col):
-        # Обработчик события щелчка на поле ввода
-        entry = event.widget
-        entry.config(fg="black")
+                # Установка цвета текста
+                cell_entry.config(fg=text_color)
 
-    def on_key_press(self, event, row, col):
-        # Обработчик события нажатия клавиши на поле ввода
-        entry = event.widget
-        value = entry.get()
-        if value.isdigit() and 1 <= int(value) <= 9:
-            self.make_move(row, col, int(value))
-            self.update_board()
+                self.entries[i][j] = cell_entry
 
-    def make_move(self, row, col, value):
-        if self.game.make_move(row, col, value):
-            self.update_board()
-            if not self.game.check_winner() and not self.game.is_board_full():
-                self.root.after(500, self.computer_move)
+                # Разграничение блоков 3x3
+                if i % 3 == 0 and i > 0:
+                    cell_entry.grid(pady=(10, 0))
+                if j % 3 == 0 and j > 0:
+                    cell_entry.grid(padx=(10, 0))
 
-    def computer_move(self):
-        if not self.game.is_board_full() and not self.game.check_winner():
-            empty_positions = [(i, j) for i in range(9) for j in range(9) if self.game.board[i][j] == 0]
-            computer_choice = random.choice(empty_positions)
-            self.game.make_move(computer_choice[0], computer_choice[1], 2)
-            self.update_board()
+        check_button = tk.Button(frame, text="Check", command=self.check_solution, font=("Arial", 16))
+        check_button.grid(row=9, column=4, pady=20)
 
-    def update_board(self):
-        if self.game.check_winner() or self.game.is_board_full():
-            self.show_game_menu_callback()
+    def check_solution(self):
+        user_solution = []
+        for i in range(9):
+            row_values = []
+            for j in range(9):
+                value = self.entries[i][j].get()
+                row_values.append(int(value) if value else 0)
+            user_solution.append(row_values)
+
+        if any(any(val != 0 for val in row) for row in user_solution):
+            if self.is_solution_valid(user_solution):
+                messagebox.showinfo("Congratulations!", "You solved the Sudoku puzzle!")
+            else:
+                messagebox.showerror("Incorrect Solution", "Your solution is incorrect. Please try again.")
         else:
-            for i in range(9):
-                for j in range(9):
-                    value = self.game.board[i][j]
-                    entry = self.get_entry(i, j)
-                    entry.delete(0, tk.END)
-                    entry.insert(0, str(value))
+            messagebox.showerror("No Input", "Please fill in at least one cell before checking.")
 
-    def get_entry(self, row, col):
-        for widget in self.frame.winfo_children():
-            if widget.grid_info()["row"] == row and widget.grid_info()["column"] == col:
-                return widget
+    def is_solution_valid(self, solution):
+        # Проверка по строкам и столбцам
+        for i in range(9):
+            if not self.is_unit_valid(solution[i]) or not self.is_unit_valid([solution[j][i] for j in range(9)]):
+                return False
+
+        # Проверка по блокам 3x3
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                block = [solution[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                if not self.is_unit_valid(block):
+                    return False
+
+        return True
+
+    def is_unit_valid(self, unit):
+        # Проверка, что все элементы в строке/столбце/блоке уникальны
+        seen = set()
+        for num in unit:
+            if num != 0:
+                if num in seen:
+                    return False
+                seen.add(num)
+        return True
+
 
 class BrainCrushApp:
     def __init__(self, root):
-        self.db = Database()
         self.root = root
         self.root.title("Brain Crush")
         self.root.geometry("800x600")
 
-        self.show_welcome_frame()
-        self.root.mainloop()
+        # Показываем правила игры перед началом игры
+        self.show_rules()
 
-    def show_welcome_frame(self):
-        title_label = tk.Label(self.root, text="BRAIN CRUSH", font=("Arial", 30))
-        title_label.pack(pady=50)
+    def show_rules(self):
+        rules_text = (
+            "Правила игры в судоку:\n\n"
+            "1. Заполните каждую ячейку числом от 1 до 9 так, чтобы каждая цифра встречалась "
+            "в каждой строке, в каждом столбце и в каждом блоке 3x3.\n"
+            "2. Заполните все ячейки, чтобы завершить игру."
+        )
 
-        subtitle_label = tk.Label(self.root, text="Нажмите Enter для продолжения", font=("Arial", 10))
-        subtitle_label.pack(pady=20)
+        rules_label = tk.Label(self.root, text=rules_text, font=("Arial", 18), justify="left")
+        rules_label.pack(padx=20, pady=20)
 
-        self.root.bind("<Return>", lambda event: self.show_main_menu())
+        start_button = tk.Button(self.root, text="Начать игру", command=self.start_sudoku_game, font=("Arial", 16))
+        start_button.pack(pady=20)
 
-    def show_main_menu(self):
+    def start_sudoku_game(self):
+        # Удаляем правила и начинаем игру
         for widget in self.root.winfo_children():
             widget.destroy()
+        sudoku_game = SudokuGame(self.root, self.show_rules)
 
-        main_menu_frame = tk.Frame(self.root)
-        main_menu_frame.pack()
-
-        title_label = tk.Label(main_menu_frame, text="BRAIN CRUSH", font=("Arial", 30))
-        title_label.pack(pady=20)
-
-        def tic_tac_toe_clicked():
-            TicTacToeApp(self.root, self.db, self.show_game_menu)
-
-        def sudoku_clicked():
-            SudokuApp(self.root, self.db, self.show_game_menu)
-
-        def exit_clicked():
-            self.root.destroy()
-
-        tic_tac_toe_button = tk.Button(main_menu_frame, text="Крестики-нолики", command=tic_tac_toe_clicked)
-        tic_tac_toe_button.pack(pady=10)
-
-        sudoku_button = tk.Button(main_menu_frame, text="Судоку", command=sudoku_clicked)
-        sudoku_button.pack(pady=10)
-
-        exit_button = tk.Button(main_menu_frame, text="Выйти из приложения", command=exit_clicked)
-        exit_button.pack(pady=10)
-
-    def show_game_menu(self):
-        for widget in self.root.winfo_children():
-            widget.destroy()
-
-        game_menu_frame = tk.Frame(self.root)
-        game_menu_frame.pack()
-
-        title_label = tk.Label(game_menu_frame, text="Выберите игру", font=("Arial", 30))
-        title_label.pack(pady=20)
-
-        def tic_tac_toe_clicked():
-            TicTacToeApp(self.root, self.db, self.show_game_menu)
-
-        def sudoku_clicked():
-            SudokuApp(self.root, self.db, self.show_game_menu)
-
-        def exit_clicked():
-            self.root.destroy()
-
-        tic_tac_toe_button = tk.Button(game_menu_frame, text="Крестики-нолики", command=tic_tac_toe_clicked)
-        tic_tac_toe_button.pack(pady=10)
-
-        sudoku_button = tk.Button(game_menu_frame, text="Судоку", command=sudoku_clicked)
-        sudoku_button.pack(pady=10)
-
-        exit_button = tk.Button(game_menu_frame, text="Выйти из программы", command=exit_clicked)
-        exit_button.pack(pady=10)
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = BrainCrushApp(root)
+    root.mainloop()
