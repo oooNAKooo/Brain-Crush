9bdbe4ffd6a19c5d9a1f488b4b031112ddbe3802
Samuel <121157661+oooNAKooo@users.noreply.github.com>
2024-02-16 15:30:56 +0300

    upgrade_database.py

--- Code/database.py
+++ Code/database.py
@@ -6,14 +6,16 @@ class Database:
     def __init__(self):
         self.conn = sqlite3.connect('users.db')
         self.cursor = self.conn.cursor()
-
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                username TEXT,
-                password TEXT
-            )
-        ''')
+    
+    def create_users_table(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                                id INTEGER PRIMARY KEY,
+                                username TEXT NOT NULL UNIQUE,
+                                password TEXT NOT NULL,
+                                snake_score INTEGER DEFAULT 0,
+                                tic_tac_toe_wins INTEGER DEFAULT 0,
+                                sudoku_solved INTEGER DEFAULT 0
+                            )''')
         self.conn.commit()
 
     def add_user(self, username, password):
@@ -28,6 +30,23 @@ class Database:
         self.cursor.execute('SELECT * FROM users')
         return self.cursor.fetchall()
 
+    def update_snake_score(self, username, score):
+        self.cursor.execute('UPDATE users SET snake_score = ? WHERE username = ?', (score, username))
+        self.conn.commit()
+
+    def update_tic_tac_toe_wins(self, username, wins):
+        self.cursor.execute('UPDATE users SET tic_tac_toe_wins = ? WHERE username = ?', (wins, username))
+        self.conn.commit()
+
+    def update_sudoku_solved(self, username, solved_count):
+        self.cursor.execute('UPDATE users SET sudoku_solved = ? WHERE username = ?', (solved_count, username))
+        self.conn.commit()
+
+    def get_tic_tac_toe_score(self, username):
+        self.cursor.execute('SELECT tic_tac_toe_wins FROM users WHERE username = ?', (username,))
+        return self.cursor.fetchone()[0]  # возвращает количество побед в крестики-нолики
+
+    
     def delete_user(self, username):
         self.cursor.execute('DELETE FROM users WHERE username = ?', (username,))
         self.conn.commit()
