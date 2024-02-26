76d03ff5a799ee3c821b10de1cf66a5eb305955d
Samuel <121157661+oooNAKooo@users.noreply.github.com>
2024-02-26 21:27:04 +0300

    add_stats_to_some_games

--- Code/statistics_window.py
+++ Code/statistics_window.py
@@ -22,6 +22,12 @@ class StatisticsWindow(tk.Toplevel):
         self.sudoku_button = tk.Button(self, text="Судоку", command=self.show_sudoku_statistics)
         self.sudoku_button.pack(pady=5)
 
+        self.dino_button = tk.Button(self, text="Динозавр", command=self.show_dino_statistics)
+        self.dino_button.pack(pady=5)
+
+        self.race_button = tk.Button(self, text="Гонки", command=self.show_race_statistics)
+        self.race_button.pack(pady=5)
+
     def show_snake_statistics(self):
         snake_record = self.db.get_snake_score(self.username)
         messagebox.showinfo("Статистика Змейки", f"Рекорд в Змейке: {snake_record}")
@@ -30,7 +36,15 @@ class StatisticsWindow(tk.Toplevel):
         # Получаем статистику по игре "Крестики-нолики" для выбранного пользователя
         tic_tac_toe_wins = self.db.get_tic_tac_toe_score(self.username)
         messagebox.showinfo("Статистика Крестиков-ноликов", f"Количество побед в Крестики-нолики: {tic_tac_toe_wins}")
-
+        
     def show_sudoku_statistics(self):
         sudoku_solved = self.db.get_sudoku_score(self.username)
         messagebox.showinfo("Статистика Судоку", f"Количество решенных партий в Судоку: {sudoku_solved}")
+
+    def show_dino_statistics(self):
+        dino_run = self.db.get_dino_score(self.username)
+        messagebox.showinfo("Статистика Динозавра", f"Количество решенных партий в Судоку: {dino_run}")
+
+    def show_race_statistics(self):
+        race_run = self.db.get_race_score(self.username)
+        messagebox.showinfo("Статистика Гонок", f"Количество набранных очков: {race_run}")
