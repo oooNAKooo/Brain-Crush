3ba8be923ccf1431b5d60ba855ac5e6b20ba3f99
Samuel <121157661+oooNAKooo@users.noreply.github.com>
2024-02-26 21:25:14 +0300

    add_new_main.py
    I was tired, so I left everything

--- Code/main.py
+++ Code/main.py
@@ -1,16 +1,19 @@
 # main.py
 import tkinter as tk
-import pygame
 from tkinter import messagebox
 from database import Database
 from tictactoe import TicTacToeApp
 from admin import AdminApp
 from snake import SnakeGame
+from dino import DinoGame
+from race import RaceGame
 from sudoku import SudokuGame  # Импортируем класс SudokuGame из файла sudoku.py
 from statistics_window import StatisticsWindow  # Импорт класса StatisticsWindow
 
-# Инициализация Pygame
+# Инициализация Pygame перед созданием объекта BrainCrushApp
+import pygame
 pygame.init()
+
 
 
 class BrainCrushApp:
@@ -35,6 +38,7 @@ class BrainCrushApp:
     def show_admin_menu(self):
         # Передайте объект Database при создании AdminApp
         AdminApp(self.root, self.db, lambda: self.show_main_menu())
+
 
     def show_main_menu(self):
         for widget in self.root.winfo_children():
@@ -99,7 +103,7 @@ class BrainCrushApp:
                 elif not user:
                     messagebox.showwarning("Предупреждение", "Пользователя с таким именем не существует!")
                 else:
-                    messagebox.showwarning("Предупреждение", "Имя пользователя и/или пароль введены не верно!")
+                    messagebox.showwarning("Ошибка", "Имя пользователя и/или пароль введены не верно!")
 
         login_button = tk.Button(login_frame, text="Войти", command=login_user)
         login_button.pack(pady=20)
@@ -161,17 +165,24 @@ class BrainCrushApp:
         game_menu_frame = tk.Frame(self.root)
         game_menu_frame.pack()
 
-        title_label = tk.Label(game_menu_frame, text="Выберите игру", font=("Arial", 30))
+        title_label_text = f"Выберите игру, {username}"
+        title_label = tk.Label(game_menu_frame, text=title_label_text, font=("Arial", 30))
         title_label.pack(pady=20)
 
         def tic_tac_toe_clicked():
-            TicTacToeApp(self.root, self.db, lambda: self.show_game_menu(username))
+            TicTacToeApp(self.root, self.db, lambda: self.show_game_menu_callback, username)
 
         def snake_clicked():
-            self.start_snake_game()
+            self.start_snake_game(username)
 
         def sudoku_clicked():
             self.start_sudoku_game()
+
+        def dino_clicked():
+            self.start_dino_game()
+
+        def race_clicked():
+            self.start_race_game()
 
         def exit_clicked():
             self.root.destroy()
@@ -188,6 +199,12 @@ class BrainCrushApp:
         sudoku_button = tk.Button(game_menu_frame, text="Судоку", command=sudoku_clicked)
         sudoku_button.pack(pady=10)
 
+        sudoku_button = tk.Button(game_menu_frame, text="Динозавр", command=dino_clicked)
+        sudoku_button.pack(pady=10)
+
+        sudoku_button = tk.Button(game_menu_frame, text="Гонки", command=race_clicked)
+        sudoku_button.pack(pady=10)
+
         statistics_button = tk.Button(game_menu_frame, text="Статистика", command=statistic_clicked)
         statistics_button.pack(pady=10)
 
@@ -196,17 +213,24 @@ class BrainCrushApp:
 
         exit_button = tk.Button(game_menu_frame, text="Выйти из программы", command=exit_clicked)
         exit_button.pack(pady=10)
-    
+
     def show_statistics(self, username):
         StatisticsWindow(self.root, self.db, username, lambda: self.show_game_menu_callback)
-
-    def start_snake_game(self):
-        snake_game = SnakeGame()
-        snake_game.run()
 
     def start_sudoku_game(self):
         sudoku_game = SudokuGame(self.root, self.show_main_menu)
         sudoku_game.draw_board()
+
+    def start_snake_game(self, username):
+        snake_game = SnakeGame(self.db, username)
+        snake_game.run()
+
+    def start_dino_game(self):
+        DinoGame(self, self.root, self.db, self.username)
+
+    def start_race_game(self):
+        RaceGame()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
